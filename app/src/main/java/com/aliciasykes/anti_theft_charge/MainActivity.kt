139538323389daffb3790836ab3566adac23fb25
Alicia Sykes <sykes.alicia@gmail.com>
2017-10-28 15:46:11 +0100

    Starting from scratch... in Kotlin

---
+++ app/src/main/java/com/aliciasykes/anti_theft_charge/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.aliciasykes.anti_theft_charge
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
