53c813dbacb06dbeedaaa6a53cf1d6a383834c24
Alicia Sykes <sykes.alicia@gmail.com>
2017-10-30 22:32:02 +0000

    Included libs for implementing toggle button

--- app/src/main/java/com/aliciasykes/anti_theft_charge/MainActivity.kt
+++ app/src/main/java/com/aliciasykes/anti_theft_charge/MainActivity.kt
@@ -2,11 +2,24 @@ package com.aliciasykes.anti_theft_charge
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.dx.dxloadingbutton.lib.LoadingButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lb = findViewById<View>(R.id.toggleButton) as LoadingButton
+        lb.setOnClickListener(View.OnClickListener() {
+
+            fun onClick(view: View) {
+                lb.startLoading() //start loading
+            }
+        })
+
+
+
     }
 }
