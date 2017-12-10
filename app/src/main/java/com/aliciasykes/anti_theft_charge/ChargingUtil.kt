cc7fedb14978459912746d0a2f74590fee8513ea
Alicia Sykes <sykes.alicia@gmail.com>
2017-12-10 18:29:15 +0000

    Fix no constructor error

--- app/src/main/java/com/aliciasykes/anti_theft_charge/ChargingUtil.kt
+++ app/src/main/java/com/aliciasykes/anti_theft_charge/ChargingUtil.kt
@@ -24,16 +24,17 @@ class ChargingUtil{
         val plugged = intent!!.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1)
         return plugged == BatteryManager.BATTERY_PLUGGED_AC || plugged == BatteryManager.BATTERY_PLUGGED_USB
     }
+}
 
-    inner class PlugInControlReceiver : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            val action = intent.action
 
-            if (action == Intent.ACTION_POWER_CONNECTED) {
-                // Do something when power connected
-            } else if (action == Intent.ACTION_POWER_DISCONNECTED) {
-                // Do something when power disconnected
-            }
+class PlugInControlReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val action = intent.action
+
+        if (action == Intent.ACTION_POWER_CONNECTED) {
+            // Do something when power connected
+        } else if (action == Intent.ACTION_POWER_DISCONNECTED) {
+            // Do something when power disconnected
         }
     }
 }
