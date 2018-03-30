f82a024d123d6f59c886180838202967065864f3
Alicia Sykes <sykes.alicia@gmail.com>
2018-03-30 21:01:25 +0100

    Alarm functioanlity done

--- app/src/main/java/com/aliciasykes/anti_theft_charge/AlarmUtil.kt
+++ app/src/main/java/com/aliciasykes/anti_theft_charge/AlarmUtil.kt
@@ -1,25 +1,60 @@
 package com.aliciasykes.anti_theft_charge
 
 import android.media.MediaPlayer
+import android.media.AudioManager
+import android.content.Context.AUDIO_SERVICE
+
+
 
 class AlarmUtil(_mainActivity: MainActivity) {
 
     private var mainActivity: MainActivity = _mainActivity
     private lateinit var mp: MediaPlayer
+    private lateinit var am: AudioManager
+    private var previousVolumeLevel: Int = 0
 
-    fun initMp(){
-        mp = MediaPlayer.create(mainActivity, R.raw.alarm1)
-    }
-
+    /**
+     * BEEP BEEP BEEP BEEP BEEP BEEP....
+     */
     fun soundTheAlarm(){
-        initMp()
+        mp = MediaPlayer.create(mainActivity, R.raw.alarm1)
+        setVolumeToMax()
         mp.start()
     }
 
+    /**
+     * Ah, silence!
+     */
     fun stopTheAlarm(){
-        if (::mp.isInitialized) {
+        if (::mp.isInitialized){
             mp.stop()
         }
+        setVolumeBackToDefault()
     }
 
+    /**
+     * Sets the device media volume to max, when the alarm starts
+     * Also records what the previous volume was, so we can put it back
+     */
+    private fun setVolumeToMax(){
+
+        am = mainActivity.getSystemService(AUDIO_SERVICE) as AudioManager
+
+        previousVolumeLevel = am.getStreamVolume(AudioManager.STREAM_MUSIC)
+
+        am.setStreamVolume(
+                AudioManager.STREAM_MUSIC,
+                am.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
+                0)
+    }
+
+    /**
+     * Puts device back to previous volume, when alarm stops
+     */
+    private fun setVolumeBackToDefault(){
+        if (previousVolumeLevel != 0 && ::am.isInitialized){
+            am.setStreamVolume(AudioManager.STREAM_MUSIC, previousVolumeLevel, 0)
+        }
+
+    }
 }
