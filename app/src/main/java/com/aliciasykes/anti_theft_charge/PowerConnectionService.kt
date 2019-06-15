ccd5f69dc2f9a0d2333556b1c7f9a20e30c5a61b
Alicia Sykes <alicia.sykes@protonmail.com>
2019-06-15 23:23:20 +0100

    Refactored imports, removed logs and commented out code blocks

--- app/src/main/java/com/aliciasykes/anti_theft_charge/PowerConnectionService.kt
+++ app/src/main/java/com/aliciasykes/anti_theft_charge/PowerConnectionService.kt
@@ -70,10 +70,10 @@ class PowerConnectionService : Service() {
         catch (e: Throwable) {
             // Do fuck all
         }
-//        val broadcastIntent = Intent()
-//        broadcastIntent.action = "restartservice"
-//        broadcastIntent.setClass(this, Restarter::class.java)
-//        this.sendBroadcast(broadcastIntent)
+        val broadcastIntent = Intent()
+        broadcastIntent.action = "restartservice"
+        broadcastIntent.setClass(this, Restarter::class.java)
+        this.sendBroadcast(broadcastIntent)
     }
 
     override fun onBind(intent: Intent): IBinder? {
--- app/src/main/java/com/aliciasykes/anti_theft_charge/Restarter.kt
+++ app/src/main/java/com/aliciasykes/anti_theft_charge/Restarter.kt
@@ -1,18 +1,12 @@
 package com.aliciasykes.anti_theft_charge
 
-import android.content.Intent
-import android.os.Build
-import android.widget.Toast
 import android.content.BroadcastReceiver
 import android.content.Context
-import android.util.Log
-
+import android.content.Intent
+import android.os.Build
 
 class Restarter : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        Log.i("Broadcast Listened", "Service tried to stop")
-        Toast.makeText(context, "Service restarted", Toast.LENGTH_SHORT).show()
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             context.startForegroundService(Intent(context, PowerConnectionService::class.java))
         } else {
